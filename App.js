5e9ec36387c908e9414973546a3a0874aa471b57
MakarPustovalov <60263005+MakarPustovalov@users.noreply.github.com>
2021-05-24 12:43:30 +0300

    Added bodyfat calculation

--- App.js
+++ App.js
@@ -1,59 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
   const [weight, setWeight] = useState(0);
   const [height, setHeight] = useState(0);
-  const [BMI, setBMI] = useState(0);
+  const [neck, setNeck] = useState(0);
+  const [waist, setWaist] = useState(0);
+  const [bodyParams, setBodyParams] = useState({});
 
-  const calcBMI = (w, h) => {
-    if (w <= 0 || h <= 0) return alert('Введите рост и вес')
-    let result = w / Math.pow((h / 100), 2)
+  const calcBMI = (data) => {
+    let {height, weight} = data
+    if (weight <= 0 || height <= 0) return alert('Введите все параметры')
+
+    let result = weight / Math.pow((height / 100), 2)
     result = result.toFixed(2)
-    setBMI(result)
+
+    return result
+  }
+
+  const calcBFPMale = (data) => {
+    let {height, neck, waist} = data
+    if (neck <= 0 || height <= 0 || waist <= 0) return alert('Введите все параметры')
+
+    let result =	(495 / ((1.0324 - 0.19077 * Math.log10(waist - neck) ) + 0.15456 * Math.log10(height))) - 450
+    result = result.toFixed(2)
+
+    return result
+  }
+
+  const calcBodyParams = (data) => {
+    // if ((neck && waist && height && weight) == 0) return alert('Введите все параметры')
+    const BMI = calcBMI(data)
+    const BFPMale = calcBFPMale(data)
+    setBodyParams({BMI, BFPMale})
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Рассчёт ИМТ</Text>
-      <Text style={styles.text}>Ваш вес (кг)</Text>
-      <TextInput style={styles.input} onChangeText={(value) => {setWeight(value)}}></TextInput>
-      <Text style={styles.text}>Ваш рост (см)</Text>
-      <TextInput style={styles.input} onChangeText={(value) => {setHeight(value)}}></TextInput>
-      <TouchableOpacity style={styles.button} onPress={() => calcBMI(weight, height)}><Text style={styles.buttonText}>Рассчитать ИМТ</Text></TouchableOpacity>
-      {BMI > 0 ? <Text style={styles.result}>Ваш BMI: {BMI}</Text> : false}
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Рассчёт BMI/BF/FFMI</Text>
+
+        <View style={styles.row}>
+
+          <View>
+            <Text style={styles.text}>Вес (кг)</Text>
+            <TextInput style={styles.input} onChangeText={(value) => {setWeight(value)}}></TextInput>
+          </View>
+
+          <View>
+            <Text style={styles.text}>Рост (см)</Text>
+            <TextInput style={styles.input} onChangeText={(value) => {setHeight(value)}}></TextInput>
+          </View>
+
+        </View>
+
+        <View style={styles.row}>
+
+          <View>
+            <Text style={styles.text}>Шея (см)</Text>
+            <TextInput style={styles.input} onChangeText={(value) => {setNeck(value)}}></TextInput>
+          </View>
+
+          <View>
+            <Text style={styles.text}>Талия (см)</Text>
+            <TextInput style={styles.input} onChangeText={(value) => {setWaist(value)}}></TextInput>
+          </View>
+
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => calcBodyParams({weight, height, neck, waist})}><Text style={styles.buttonText}>Рассчитать</Text></TouchableOpacity>
+        {bodyParams.BMI > 0 ? <Text style={styles.result}>Ваш BMI: {bodyParams.BMI}</Text> : false}
+        {bodyParams.BFPMale > 0 ? <Text style={styles.result}>Ваш ППЖ: {bodyParams.BFPMale}%</Text> : false}
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 70,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    marginBottom: 10
+    marginBottom: 7
+  },
+  row: {
+    width: 300,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 30,
   },
   title: {
-    fontSize: 36,
-    marginBottom: 70
+    textAlign: 'center',
+    fontSize: 30,
+    marginBottom: 50
   },
   input: {
     paddingLeft: 10,
-    width: 150,
+    width: 100,
     height: 40,
-    marginBottom: 30,
+    marginBottom: 20,
     borderColor: '#333333',
     borderWidth: 1,
     borderStyle: 'solid',
     borderRadius: 4
   },
   button: {
+    marginTop: 8,
+    marginBottom: 15,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
@@ -68,7 +129,8 @@ const styles = StyleSheet.create({
     fontWeight: "500"
   },
   result: {
-    marginTop: 20,
+    fontWeight: "700",
+    marginTop: 15,
     fontSize: 18
   }
 });
